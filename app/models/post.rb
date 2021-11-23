9210d8e96e9d85093c524c64c4c3ce7ab20b2d2e
Yeison <enmanuelquiterios@gmail.com>
2021-11-23 12:28:51 -0500

    Need to verify Inlusion Validations and Customs Validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :title, acceptace: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction)}
+    validates :category, inclusion: {in: %w(Non-Fiction)}
+
+
 end
